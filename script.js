d9931a205c81367e1ddb3fa16514e3cc37cbb012
RajuMailapalli <154442214+RajuMailapalli@users.noreply.github.com>
2025-03-20 03:24:46 +0530

    Update script.js
    Add modifications

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById("darkModeToggle").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
 });
+
+document.getElementById("projectsBtn").addEventListener("click", function() {
+    document.getElementById("projects").classList.toggle("hidden");
+});
+
+document.getElementById("contactBtn").addEventListener("click", function() {
+    document.getElementById("contact").classList.toggle("hidden");
+});
